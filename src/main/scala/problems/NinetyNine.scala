2247f239cdcbe658c3e2475027d217168a385258
Chris Cody <3807221+aussiechris@users.noreply.github.com>
2019-04-06 22:33:50 +1100

    Fixed some tests, added some more

--- src/main/scala/problems/NinetyNine.scala
+++ src/main/scala/problems/NinetyNine.scala
@@ -6,67 +6,71 @@ object NinetyNine {
 
   // Find the last element of a list.
 
-  def lastElement[T](list:List[T]):T ={
-    ???
+  def lastElement[T](list: List[T]): T = {
+    list.last
   }
 
   // Find the last but one element of a list.
-  def secondLastElement[T](list:List[T]):T ={
-    ???
+  def secondLastElement[T](list: List[T]): T = {
+    if (list.length > 1) {
+      list(list.length - 2)
+    } else throw new NoSuchElementException
   }
 
   // Find the Kth element of a list.
-  def kthElement[T](list:List[T], element:Int):T ={
-    ???
+  def kthElement[T](list: List[T], element: Int): T = {
+    if (0 <= element && element < list.length) {
+      list(element)
+    } else throw new NoSuchElementException
+
   }
 
   // Find the number of elements of a list.
-  def listLength[T](list:List[T]):Int ={
-    ???
+  def listLength[T](list: List[T]): Int = {
+    list.length
   }
 
   // Reverse a list.
-  def reverseList[T](list:List[T]):List[T] ={
+  def reverseList[T](list: List[T]): List[T] = {
+    list.reverse
   }
 
   // Find out whether a list is a palindrome.
-  def isPalindrome[T](list:List[T]):Boolean ={
-    ???
+  def isPalindrome[T](list: List[T]): Boolean = {
+    list == list.reverse
   }
 
   // Flatten a nested list structure.
-  def flatenList[T](list:List[T]):List[Any]={
+  def flattenList[T](list: List[T]): List[T] = {
     ???
   }
 
   // Eliminate consecutive duplicates of list elements.
   // If a list contains repeated elements they should be replaced with a single copy of the element.
   // The order of the elements should not be changed.
-  def noConsecutiveDupes[T](list:List[T]):List[T]={
+  def noConsecutiveDupes[T](list: List[T]): List[T] = {
     ???
   }
 
   // Pack consecutive duplicates of list elements into sublists.
   // If a list contains repeated elements they should be placed in separate sublists.
-  def dupePacker[T](list:List[T]):List[List[T]] ={
+  def dupePacker[T](list: List[T]): List[List[T]] = {
     ???
   }
 
   // Run-length encoding of a list.
   // Use the result of dupePacker to implement the so-called run-length encoding data compression method.
   // Consecutive duplicates of elements are encoded as tuples (N, E) where N is the number of duplicates of the element E.
-  def encode[T](list:List[T]):List[(Int, T)] ={
+  def encode[T](list: List[T]): List[(Int, T)] = {
     ???
   }
 
   // Modified run-length encoding
   // Modify the result of encode in such a way that if an element has no duplicates it is simply
   // copied into the result list. Only elements with duplicates are transferred as (N, E) terms.
-  def modifiedEncode[T](list:List[T]):List[Any] ={
+  def modifiedEncode[T](list: List[T]): List[Any] = {
     ???
   }
-
-
 
 
 }
--- src/test/scala/problems/NinetyNineSpec.scala
+++ src/test/scala/problems/NinetyNineSpec.scala
@@ -7,9 +7,10 @@ class NinetyNineSpec extends FlatSpec with Matchers {
   import NinetyNine._
 
   "lastElement" should "return the last element of a list" in {
-    lastElement(List(1,2,3,4,5)) should be (5)
-    lastElement(('a' to 'z').toList) should be ('z')
-    lastElement(List("adam","austin","is","no","good")) should be ("good")
+    lastElement(List(1, 1, 2, 3, 5, 8)) should be(8)
+    lastElement(List(1, 2, 3, 4, 5)) should be(5)
+    lastElement(('a' to 'z').toList) should be('z')
+    lastElement(List("adam", "austin", "is", "no", "good")) should be("good")
 
     assertThrows[NoSuchElementException] {
       lastElement(List())
@@ -17,6 +18,7 @@ class NinetyNineSpec extends FlatSpec with Matchers {
   }
 
   "secondLastElement" should "return the second last element in a list" in {
+    secondLastElement(List(1, 1, 2, 3, 5, 8)) should be(5)
     secondLastElement((1 to 10).toList) should be(9)
     secondLastElement(('a' to 'z').toList) should be('y')
     secondLastElement(List("adam", "austin", "is", "no", "good")) should be("no")
@@ -26,29 +28,46 @@ class NinetyNineSpec extends FlatSpec with Matchers {
     }
   }
 
-    "kthElement" should "return the kth element from a list" in {
-      kthElement((1 to 10).toList, 4) should be (3)
-      kthElement(('a' to 'z').toList, 7) should be ('f')
+  "kthElement" should "return the kth element from a list" in {
+    kthElement(List(1, 1, 2, 3, 5, 8), 2) should be(2)
+    kthElement((1 to 10).toList, 3) should be(4)
+    kthElement(('a' to 'z').toList, 7) should be('h')
 
-      assertThrows[NoSuchElementException] {
-        kthElement(List(), 0)
-      }
+    assertThrows[NoSuchElementException] {
+      kthElement(List(), 0)
     }
+  }
 
-    "listLength" should "return the length of a list" in {
-      listLength((1 to 10).toList) should be (10)
-      listLength(('a' to 'z').toList) should be (26)
-      listLength(List()) should be (0)
-      listLength(List(('a' to 'z').toList, (1 to 10).toList)) should be (2)
-    }
+  "listLength" should "return the length of a list" in {
+    listLength((1 to 10).toList) should be(10)
+    listLength(('a' to 'z').toList) should be(26)
+    listLength(List()) should be(0)
+    listLength(List(('a' to 'z').toList, (1 to 10).toList)) should be(2)
+  }
 
-    "reverseList" should "return a list with elements reversed" in {
-      reverseList((1 to 10).toList) should be (List(10,9,8,7,6,5,4,3,2,1))
-      reverseList(List('a', 'b', 'c')) should be (List('c', 'b', 'a'))
-      reverseList(List(List('a', 'b'), List(1 , 2, 3))) should be (List(List(1,2,3), List('a', 'b')))
-      reverseList(List()) should be (List())
+  "reverseList" should "return a list with elements reversed" in {
+    reverseList((1 to 10).toList) should be(List(10, 9, 8, 7, 6, 5, 4, 3, 2, 1))
+    reverseList(List('a', 'b', 'c')) should be(List('c', 'b', 'a'))
+    reverseList(List(List('a', 'b'), List(1, 2, 3))) should be(List(List(1, 2, 3), List('a', 'b')))
+    reverseList(List()) should be(List())
 
-    }
+  }
+
+  "isPalindrome" should "determine palindromes correctly" in {
+    isPalindrome(List(1, 2, 3, 2, 1)) should be(true)
+    isPalindrome(List()) should be(true)
+    isPalindrome(List(1)) should be(true)
+    isPalindrome(List(1, 1)) should be(true)
+    isPalindrome(List(1, 2)) should be(false)
+    isPalindrome(List(1, 2, 1, 1)) should be(false)
+    isPalindrome((1 to 50).toList ++ (1 to 50).reverse.toList) should be(true)
+  }
+
+  "flattenList" should "determine palindromes correctly" in {
+    flattenList(List(List(1, 1), 2, List(3, List(5, 8)))) should be(List(1, 1, 2, 3, 5, 8))
+    flattenList(List(List(8, List(7), List(6), List(List(5), List(4, List(3)))))) should be(List(8, 7, 6, 5, 4, 3))
+    flattenList(List(List(), List(List()))) should be(List())
+  }
 
 
 }
